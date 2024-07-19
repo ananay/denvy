f3bebda448d04d8eda11f5489c46667299a7bee8
ananay <i@ananayarora.com>
2024-07-18 18:36:13 -0700

    new version

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ const main = async () => {
         .name("denvy")
         .description("Manage environment variables and .env files")
         .usage("[command] [options]")
-        .version("0.0.1")
+        .version("0.0.1-alpha")
 
     program
         .command('sample')
